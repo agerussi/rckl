27872468b213ea7b64b015b4152a60ca59601ae8
Alexandre Gerussi <alexandre.gerussi@free.fr>
2013-02-26 00:19:22 +0100

    chgt de politique sur les connexions multiples
    En cas de connexion multiple, un membre est inséré plusieurs fois
    dans chat_members.
    En cas de déconnexion, il est supprimé une seule fois.
    Ainsi on tient compte du nombre de connexions réellement en oeuvre.
    Le script chat_deconnexion.php doit supprimer chat_messages dès qu'il
    n'y a plus de connectés.
    *Cette partie bugge probablement à cause de mon comptage*

--- chat_deconnexion.php
+++ chat_deconnexion.php
@@ -1,15 +1,29 @@
 <?php 
 //////////////////////////////////////////////////////////
 // script AJAX supprimant l'utilisateur de chat_members //
+// et vidant la liste des messages le cas échéant       //
 //////////////////////////////////////////////////////////
   session_start(); 
-  //teste si l'utilisateur est connecté
+  // teste si l'utilisateur est connecté
   if (!isset($_SESSION['userid']) || empty($_SESSION['userid'])) exit(0);
 
   require("dbconnect.php");
 
   // supprime l'utilisateur de la liste des connectés au chat
-  $query="DELETE FROM chat_members WHERE id=".$_SESSION['userid'];
+  $query="DELETE FROM chat_members WHERE id=".$_SESSION['userid']." LIMIT 1";
   mysql_query($query, $db) or die("Erreur lors de la suppression du membre de chat_members: ".mysql_error());
+
+  // compte le nombre de connectés restants
+  $query="SELECT COUNT(*) FROM chat_members";
+  $result=mysql_query($query, $db) or die("Erreur lors du comptage du nombre de membres dans chat_members: ".mysql_error());
+  $row=mysql_fetch_row($result);
+  $n=$row[0];
+ 
+  // s'il ne reste plus personne, on efface la liste des messages
+  if ($n==0) {
+    $query="DELETE * FROM chat_messages";
+    mysql_query($query, $db) or die("Erreur lors de la suppression des messages de chat_messages: ".mysql_error());
+  }
+   
 ?>
 
--- chatroom.php
+++ chatroom.php
@@ -22,19 +22,20 @@
 <?php
 // début du programme spécifique 'chatroom'
 // on ajoute l'utilisateur à la liste chat_members
-// en faisant attention de ne pas avoir de double
 // le reste de la construction de la page est effectué
 // par le JS.
 
   require("dbconnect.php");
 
   // teste si le membre est déjà inséré (peut arriver si de multiples chatroom sont ouverts)
-  $query="SELECT id FROM chat_members WHERE id=".$_SESSION[userid];
+  /* $query="SELECT id FROM chat_members WHERE id=".$_SESSION[userid];
   echo $query."<br/>";
   $result=mysql_query($query, $db) or die("Erreur lors de la vérification d'unicité: ".mysql_error());
+  */
 
   // si le membre n'est pas déjà présent, l'insère dans la liste
-  if (mysql_num_rows($result)==0) {
+  //if (mysql_num_rows($result)==0) {
+  if (true) {
     $query="INSERT INTO chat_members (id, nom) VALUES(".$_SESSION[userid].",'".$_SESSION['realname']."')";
     echo $query."<br/>";
     mysql_query($query, $db) or die("Erreur lors de l'insertion du membre dans chat_members: ".mysql_error());
