9768ea1ee0c5b59aa395193f6ab22bcc9514ed7e
Alexandre Gerussi <alexandre.gerussi@free.fr>
2013-03-17 16:14:54 +0100

    enregistrerecu débuggé.
    suffisait d'enlever le || die("rrrr");

--- enregistrerecu.php
+++ enregistrerecu.php
@@ -17,21 +17,19 @@ $somme=round(100*$_POST['somme'])/100;
 require("dbconnect.php");
 
 // calcul du nouveau solde
-$query = "SELECT solde FROM membres WHERE id=".$_SESSION['userid'];
-echo "debug: ".$query."<br/>";
-$result = mysql_query($query,$db) || die("Erreur lors de la récupération du solde");
-echo "debug: ".$result."<br/>";
-$ligne = mysql_fetch_array($result);
-echo "debug: ".$ligne."<br/>";
-$solde = $ligne['solde'];
-$solde -= $somme; // nouveau solde, à sauvegarder
+$query="SELECT solde FROM membres WHERE id=".$_SESSION['userid'];
+//echo "debug: ".$query."<br/>";
+$result=mysql_query($query,$db);
+$ligne=mysql_fetch_array($result);
+$solde=$ligne['solde'];
+$solde-=$somme; // nouveau solde, à sauvegarder
 
 $query = "UPDATE membres SET solde=".$solde." WHERE id=".$_SESSION['userid'];
 echo "debug: ".$query."<br/>";
 //mysql_query($query,$db) || die("Erreur lors de l'enregistrement du nouveau solde");
 
 // rajout dans l'historique
-$listevariations=$_SESSION['realname'].'(-'.$somme.'), ';
+$listevariations=$_SESSION['realname'].'(-'.$somme.')';
 $cancel=$_SESSION['userid'].','.$somme;
 echo "debug: listevariations=".$listevariations.'<br/>';
 echo "debug: cancel=".$cancel.'<br/>';
