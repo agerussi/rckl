bfb2fa479ed5ddd7dd399cfc69702936a7f742e1
Alexandre Gerussi <alexandre.gerussi@free.fr>
2013-10-20 13:33:40 +0200

    INTEGRATION: nouvelle page et entrée dans le menu
    Dans la continuité de l'action "accueil des débutants", la page
    intégration a été crée et reprend notamment certains renseignements
    préalablement contenus dans la page de présentation.
    Le stage d'intégration est pour la première fois évoqué de manière
    officielle.
    Il est également suggéré qu'une inscription en mars pourrait
    être intéressante.

---
+++ integration.php
@@ -0,0 +1,76 @@
+<?php session_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="fr">
+<head>
+<?php
+  if (isset($_GET['menu'])) require("menuh.php"); 
+  else require("head.html");
+?>
+</head>
+<body>
+<?php
+  if (isset($_GET['menu'])) require("menub.php"); 
+?>
+<h1>INTÉGRER LE GROUPE LOISIRS</h1>
+Vous avez pris votre décision: à la rentrée vous faites du kayak! 
+Voici quelques conseils pour une rentrée réussie au groupe loisirs, en particulier pour les débutants complets ou presque.
+
+<h4>Quand faut-il vous inscrire ?</h4>
+
+Même s'il est possible de vous inscrire n'importe quand dans l'année, il y a deux moments favorables: début mars ou début septembre.
+<ul>
+  <li>Début mars est une période intéressante car vous laissez derrière vous le froid de l'hiver, qui en tant que débutant est une période difficile. Le temps de découvrir un peu l'activité et les beaux jours seront là!
+  </li>
+  <li>Début septembre est le moment classique des inscriptions pour tous les clubs, et celui où la majorité des nouveaux s'inscrivent.
+Si telle est votre intention, <em>soyez près début septembre</em>: et pour cela faites vos démarches fin août. 
+En effet septembre est un mois qui reste en général doux et relativement chaud, et il serait dommage de rater ces premières séances agréables. 
+Plus vous attendrez et plus l'automne se fera sentir, le temps maussade en a découragé plus d'un(e).
+  </li>
+</ul>
+
+<h4>Les stages d'intégration</h4>
+
+Le groupe loisirs propose aux débutants fraîchement inscrits un <em>stage d'intégration sur 4 samedis après midi</em>, pendant lequel ils sont entièrement encadrés par des membres du groupe.
+Il y a deux stages prévus dans l'année: un pour les inscrits de mars, et l'autre pour ceux de septembre.
+Les dates précises, de préférence sur 4 semaines consécutives, sont négociées directement avec les stagiaires.
+<p>
+L'objectif du stage est de mettre le pied des débutants à l'étrier dans différentes formes de pratique, en leur fournissant les rudiments techniques indispensables, la connaissance et l'accès au matériel nécessaire.
+À titre indicatif, voici le contenu des journées:
+<ol>
+<li>découverte du matériel de kayak d'eau vive, travail des gestes fondamentaux sur eau plate.</li>
+<li>découverte du kayak «fitness» en eau plate sur bateau directeur.</li>
+<li>découverte de la nage en eau vive.</li>
+<li>kayak en eau vive, travail des techniques fondamentales.</li>
+</ol>
+Une sortie en rivière facile est ensuite proposée dans le prolongement du stage.
+</p>
+
+<h4>À propos de l'équipement nécessaire</h4>
+
+Le club et la base nautique vous permettent d'emprunter le matériel indispensable: gilet, jupe, casque, pagaie et kayak. 
+Il est probable qu'assez rapidement vous désiriez acheter certains de ces éléments (notamment une jupe néoprène), mais on peut utiliser ce matériel sans problème pendant la première année. 
+<br/>
+Reste donc à votre charge l'équipement corporel. 
+Voici quelques suggestions pour débuter:
+  <ul>
+    <li>Une combinaison néoprène jambes longues, pas trop épaisse (pas une combinaison de plongée). 
+L'idéal est de la prendre sans manches (bretelles), mais on peut également la prendre à manches longues pour avoir plus chaud et se passer d'un kway dans un premier temps.</li>
+    <li>Un top thermique à porter à même la peau sous la combinaison (indispensable si votre combinaison est à bretelles), à manches longues si vous voulez avoir plus chaud.</li>
+    <li>Des bottillons néoprènes avec une semelle raisonnable pour marcher dans «la brousse» ou les rochers, mais la plus souple possible, et tenant bien au pied.</li>
+<em>Tout cela se trouve facilement dans les magasins de sports traditionnels type d*c*t*l*n.</em>
+    <li>Si vous avez opté pour une combinaison à bretelles, il vous faudra encore un kway «kayak» pour se protéger du vent et des éclaboussures.
+  Pour cela il faudra probablement commander par correspondance dans un magasin spécialisé. Les kways varient selon leur solidité et leur étanchéité:
+     <ul>
+       <li>le kway basique possède la taille, les poignets et le cou fermés par du néoprène: coupe le vent, pas franchement étanche si les projections d'eau sont régulières.</li>
+       <li>le kway «standard» possède une double cheminée à la taille (système pour être étanche avec la jupe), des poignets latex: bien étanche sauf si l'on nage et/ou esquimaute très souvent. C'est le kway utilisé par la majorité des pagayeurs en eau vive.</li>
+       <li>le kway «haut de gamme» possède en plus un cou latex et peut-être davantage de «couches» et est plus solide: a priori totalement étanche. 
+Attention cependant: tout le monde ne supporte pas très bien le latex au cou, et de plus il est loin d'être indispensable pour la majorité des formes de pratique. 
+Au bout de 2 heures, il peut devenir inconfortable, et en particulier pour les randonnées en mer le mélange sel/latex peut provoquer des irritations insupportables.</li>
+     </ul>
+Pour débuter, on peut conseiller un kway basique ou une entrée de gamme «standard». 
+Il faut cependant garder à l'esprit qu'avoir un bon kway est l'assurance d'avoir chaud sur l'eau même à la mauvaise saison: on peut en effet «empiler des couches» sous le kway étanche en fonction de la température.
+  </ul>
+</ul>
+
+</body>
+</html>
