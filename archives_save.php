c44eabd7922c9ba3b97c6e48b10005c6f6c12282
Alexandre Gerussi <alexandre.gerussi@free.fr>
2014-02-06 19:31:00 +0100

    BIGFIX: gestion des magic_quotes_gpc
    dans les archives.
    Pour le reste il faudra voir au cas par cas.

--- archives_save.php
+++ archives_save.php
@@ -13,6 +13,16 @@ foreach ($_POST as $key => $value) {
     echo $key."=".$value."<br/>";
 }
 */
+// traitement anti-magic_quotes_gpc
+if (get_magic_quotes_gpc()) {
+    function stripslashes_gpc(&$value)
+    {
+        $value = stripslashes($value);
+    }
+    array_walk_recursive($_GET, 'stripslashes_gpc');
+    array_walk_recursive($_POST, 'stripslashes_gpc');
+}
+
 // récupération des données, création de l'XML
 // ===============
 $xml="";
